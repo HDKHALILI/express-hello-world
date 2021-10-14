d399f6f83d7f0ccfb1ccd2c14a515d469b1095c0
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-10-15 09:55:36 +1100

    refactor the route handler into a function

--- hello.js
+++ hello.js
@@ -45,41 +45,24 @@ app.locals.currentPathClass = (path, currentPath) => {
   return path === currentPath ? "current" : "";
 };
 
+const helloWorld = (view, language) => {
+  return (req, res) => {
+    res.render(view, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: language,
+    });
+  };
+};
+
 app.get("/", (req, res) => {
   res.redirect("/english");
 });
 
-app.get("/english", (req, res) => {
-  res.render("hello-world-english", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "en-US",
-  });
-});
-
-app.get("/french", (req, res) => {
-  res.render("hello-world-french", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "fr-FR",
-  });
-});
-
-app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "sr-Cyrl-rs",
-  });
-});
-
-app.get("/spanish", (req, res) => {
-  res.render("hello-world-spanish", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "es-ES",
-  });
-});
+app.get("/english", helloWorld("hello-world-english", "en-US"));
+app.get("/french", helloWorld("hello-world-french", "fr-FR"));
+app.get("/serbian", helloWorld("hello-world-serbian", "sr-Cyrl-rs"));
+app.get("/spanish", helloWorld("hello-world-spanish", "es-SP"));
 
 app.listen(3000, "localhost", () => {
   console.log("Listening on port 3000");
